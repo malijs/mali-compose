d87d730774b0835eb334af895db20d020349ff7a
Bojan Djurkovic <dbojan@gmail.com>
2018-01-23 13:40:21 -0400

    [skip ci] Update docs and deps

--- index.js
+++ index.js
@@ -6,13 +6,33 @@
 
 'use strict'
 
+/**
+ * Expose compositor.
+ */
+
 module.exports = compose
+
+/**
+ * Compose `middleware` returning
+ * a fully valid middleware comprised
+ * of all those which are passed.
+ *
+ * @param {Array} middleware
+ * @return {Function}
+ * @api public
+ */
 
 function compose (middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
   }
+
+  /**
+   * @param {Object} context
+   * @return {Promise}
+   * @api public
+   */
 
   return function (context, next) {
     // last called middleware #
